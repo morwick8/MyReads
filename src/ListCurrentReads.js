ec3bdfe09a29f5159e3b19e198853e821051680d
morwick8 <46034491+morwick8@users.noreply.github.com>
2019-02-01 09:11:37 -0700

    Create ListCurrentReads.js

---
+++ src/ListCurrentReads.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './App.css'
+import PropTypes from 'prop-types'
+import ShowBook from './ShowBook'
+//import sortBy from 'sort-by'
+
+class ListCurrentReads extends React.Component {
+
+ static propTypes = {
+   books: PropTypes.array.isRequired,
+   shelf: PropTypes.string.isRequired,
+   shelfName: PropTypes.string.isRequired
+}
+
+state = {
+
+}
+render() {
+	const books = this.props.books
+  	const shelf = this.props.shelf
+  	const shelfName = this.props.shelfName
+  return (
+ 	<div className="bookshelf">
+    	<h2 className="bookshelf-title">{shelfName}</h2>
+        	<div className="bookshelf-books">
+				<ol className="books-grid">
+					{books
+                	.filter((book) => book.shelf === shelf)
+  					.map((book) => (
+                     	<li key={book.title}>
+ <ShowBook book={book} />                
+</li>
+					 ))}
+					</ol>
+
+                  </div> 
+                </div>
+
+
+)
+
+}
+}
+export default ListCurrentReads
