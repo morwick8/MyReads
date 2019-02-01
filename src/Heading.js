a4f897ec91c987c1141d5893a0cdafee8df10869
morwick8 <46034491+morwick8@users.noreply.github.com>
2019-02-01 09:10:40 -0700

    Create Heading.js

---
+++ src/Heading.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Heading extends React.Component {
+  
+  render() {
+    return (
+		<div className="list-books-title">
+        	<h1>MyReads</h1>
+        </div>
+      )
+  	}
+  }
+  
+  export default Heading
